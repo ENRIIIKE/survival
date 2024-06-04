f8db1b31eb1a523f6db07fc25c0508075c06f1ec
ENRIKE <erikghast@gmail.com>
2024-06-04 17:18:50 +0200

    Update

---
+++ Survival/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private PlayerMovement _movement;
+    private Animator _animator;
+
+    public float idleCooldownTimer;
+
+    private bool _coroutinePerforming = false;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _movement = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        movementDirection direction = _movement.movementDirection;
+        switch (direction)
+        {
+            case movementDirection.Idle:
+                if (_coroutinePerforming == false)
+                {
+                    _animator.Play("Idle");
+                }
+                _animator.SetBool("Walk Horizontal", false);
+                _animator.SetBool("Walk Up", false);
+
+                break;
+
+            case movementDirection.Horizontal:
+                _animator.SetBool("Walk Horizontal", true);
+                _animator.SetBool("Walk Up", false);
+
+                StopAllCoroutines();
+                _coroutinePerforming = false;
+
+                break;
+
+            case movementDirection.Up:
+                _animator.SetBool("Walk Horizontal", false);
+                _animator.SetBool("Walk Up", true);
+
+                StopAllCoroutines();
+                _coroutinePerforming = false;
+
+                break;
+
+            case movementDirection.Diagonal:
+
+                break;
+        }
+    }
+    public void ResetIdle()
+    {
+        _animator.SetBool("Next Idle Animation", false);
+    }
+
+    public IEnumerator StartIdleTimer()
+    {
+        if (_coroutinePerforming == true)
+        {
+            yield break;
+        }
+        _coroutinePerforming = true;
+
+        yield return new WaitForSecondsRealtime(idleCooldownTimer);
+        _animator.SetBool("Next Idle Animation", true);
+
+        _coroutinePerforming = false;
+    }
+}
---
+++ Survival/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum movementDirection
+{
+    Idle,
+    Horizontal,
+    Up,
+    Down,
+    Diagonal
+}
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerInputActions _playerInput;
+    private InputAction _moveAction;
+
+    public Rigidbody2D rb;
+
+    public float moveSpeed;
+    public Vector2 vectorDirection = Vector2.zero;
+
+    public movementDirection movementDirection;
+
+    void Awake()
+    {
+        _playerInput = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        _moveAction = _playerInput.Movement.MovementAction;
+        _moveAction.Enable();
+
+    }
+    private void OnDisable()
+    {
+        _moveAction.Disable();
+    }
+
+    void Update()
+    {
+        vectorDirection = _moveAction.ReadValue<Vector2>();
+
+        if ((vectorDirection.x > 0 || vectorDirection.x < 0) && vectorDirection.y == 0)
+        {
+            movementDirection = movementDirection.Horizontal;
+
+            if (vectorDirection.x < 0)
+            {
+                transform.localScale = new Vector2(-1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector2(1, 1);
+            }
+        }
+        else if (vectorDirection.y > 0 && vectorDirection.x == 0)
+        {
+            movementDirection = movementDirection.Up;
+        }
+        else if (vectorDirection.x != 0 && vectorDirection.y != 0)
+        {
+            movementDirection = movementDirection.Diagonal;
+        }
+        else if (vectorDirection == Vector2.zero)
+        {
+            movementDirection = movementDirection.Idle;
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(vectorDirection.x, vectorDirection.y).normalized * moveSpeed;
+    }
+}
